2f8a32ba3e21ea892f8ab8f6d01ae5785c8589d6
Hien Tran <tranthaihien@192.168.1.6>
2023-11-06 21:10:09 +0700

    add javascript files

---
+++ test.js
@@ -0,0 +1,6 @@
+const customerId ={
+    id: "001",
+    fname :"Thai Hien",
+    lname :"Tran",
+};
+document.getElementById('demo').innerHTML=customerId.id +" is "+ customerId.lname+" "+customerId.fname;
